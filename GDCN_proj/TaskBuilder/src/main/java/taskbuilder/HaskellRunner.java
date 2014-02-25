14d4a8821002e6448668db4705307dd4878b4a2c
mr-ohman <ohmanjoakim@gmail.com>
2014-02-25 11:29:15 +0100

    Added PathManager usage

--- GDCN_proj/TaskBuilder/src/main/java/taskbuilder/HaskellRunner.java
+++ GDCN_proj/TaskBuilder/src/main/java/taskbuilder/HaskellRunner.java
@@ -7,16 +7,12 @@ import java.io.IOException;
  */
 public class HaskellRunner {
 
-    //TODO Handle paths in another class
-    private static final String GHC_PATH = "...";
-    private static final String JOB_CODE_PATH = "...";
-    private static final String JOB_EXECUTABLE_PATH = "...";
-    private static final String HEADER_PATH = "...";
-
     // Compiles job code
     public static void compile(String module) throws IOException {
-        String[] command = {GHC_PATH, "-o", JOB_EXECUTABLE_PATH + module, "-DMODULE=" + module, "-i" + JOB_CODE_PATH,
-                            HEADER_PATH, "-trust", "base", "-trust", "bytestring", "-trust", "binary"};
+	PathManager pathman = PathManager.getInstance();
+        String[] command = {pathman.getGhcPath(), "-o", pathman.getJobExecutablePath() + module,
+			    "-DMODULE=" + module, "-i" + pathman.getJobCodePath(), pathman.getHeaderPath(),
+			    "-trust", "base", "-trust", "bytestring", "-trust", "binary"};
         Process proc = new ProcessBuilder(command).start();
 
         if (proc.exitValue() != 0) {
