c66e49d7c1ef8f155bfe27c32d4a758cfc82bd54
mr-ohman <ohmanjoakim@gmail.com>
2014-02-25 19:24:50 +0100

    Added basic execution of tasks

--- GDCN_proj/TaskBuilder/src/main/java/taskbuilder/HaskellRunner.java
+++ GDCN_proj/TaskBuilder/src/main/java/taskbuilder/HaskellRunner.java
@@ -1,6 +1,9 @@
 package taskbuilder;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Class for compiling and executing Haskell code
@@ -8,21 +11,51 @@ import java.io.IOException;
 public class HaskellRunner {
 
     // Compiles job code
-    public static void compile(String module) throws IOException {
-	PathManager pathman = PathManager.getInstance();
+    //TODO Clean up/Prevent GHC from generating .hi and .o files for compiled programs
+    public static void compile(String module) throws IOException, InterruptedException {
+	    PathManager pathman = PathManager.getInstance();
         String[] command = {pathman.getGhcPath(), "-o", pathman.getJobExecutablePath() + module,
 			    "-DMODULE=" + module, "-i" + pathman.getJobCodePath(), pathman.getHeaderPath(),
 			    "-trust", "base", "-trust", "bytestring", "-trust", "binary"};
         Process proc = new ProcessBuilder(command).start();
 
-        if (proc.exitValue() != 0) {
+        if (proc.waitFor() != 0) {
+            //Lazy error reporting
+            IOUtils.copy(proc.getErrorStream(), System.err);
             //TODO Throw better exception (or return a false like value)
             throw new RuntimeException();
         }
     }
 
     // Executes a task
-    public static void execute(String module, String[] inputFiles) {
-        //TODO
+    public static byte[] execute(String module, String[] inputFiles) throws IOException, InterruptedException{
+        PathManager pathman = PathManager.getInstance();
+        String[] command = new String[inputFiles.length + 1];
+
+        command[0] = pathman.getJobExecutablePath() + module;
+        for (int i = 0; i < inputFiles.length; i++) {
+            command[i+1] = inputFiles[i];
+        }
+
+        Process proc = new ProcessBuilder(command).start();
+
+        if (proc.waitFor() != 0) {
+            //Lazy error reporting
+            IOUtils.copy(proc.getErrorStream(), System.err);
+            //TODO Throw better exception (or return a false like value)
+            throw new RuntimeException();
+        }
+        else {
+            //TODO Possibly wrap pure result data in a class
+            return IOUtils.toByteArray(proc.getInputStream());
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+	    PathManager.getInstance().loadFromFile("GDCN.properties");
+	    compile("Prime");
+        String[] in = {};
+	    byte[] res = execute("Prime", in);
+        System.out.println(res);
     }
 }
