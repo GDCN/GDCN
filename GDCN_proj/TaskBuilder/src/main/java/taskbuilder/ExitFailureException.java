7a348a04dc05640f27b3559053e4569ffa09c99c
mr-ohman <ohmanjoakim@gmail.com>
2014-02-26 14:36:35 +0100

    Refactor HaskellRunner to Task, added ExitFailureException

---
+++ GDCN_proj/TaskBuilder/src/main/java/taskbuilder/ExitFailureException.java
@@ -0,0 +1,15 @@
+package taskbuilder;
+
+/**
+ * Created by joakim on 2/26/14.
+ */
+public class ExitFailureException extends Exception {
+
+    public ExitFailureException(String message) {
+        super(message);
+    }
+
+    public ExitFailureException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
