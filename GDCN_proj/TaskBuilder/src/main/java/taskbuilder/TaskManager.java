71c8646a8772316b2dd3b64a5f0b938d64039cdd
Leif Schelin <schelinleif@gmail.com>
2014-02-28 15:25:55 +0100

    Added interface for communication

---
+++ GDCN_proj/TaskBuilder/src/main/java/taskbuilder/TaskManager.java
@@ -0,0 +1,20 @@
+package taskbuilder;
+
+import taskbuilder.communicationToClient.ClientFromTask;
+
+/**
+ * Created by HalfLeif on 2014-02-28.
+ */
+public class TaskManager {
+
+private final ClientFromTask client;
+
+    public TaskManager(ClientFromTask client) {
+        this.client = client;
+    }
+
+    public void startTask(){
+
+    }
+
+}
