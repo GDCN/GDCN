5161e44e1057f325af21786407653e7bf2747f74
Leif Schelin <scleif@student.chalmers.se>
2014-03-08 13:25:01 +0100

    Added explanations

--- m_script/Sybil_prob.m
+++ m_script/Sybil_prob.m
@@ -1,14 +1,27 @@
+%
+% Created by HalfLeif 2014-03-08
+%
+% Model for sybil attack on replication with "anonymous authentication"
+% for each task.
+% Calculates probability of JobOwner receiving R replica results that are
+% both Identical and Untrue (ie incomplete or partly false in any respect).
+% That is, what is the probability of JobOwner not realizing something has
+% gone very wrong with the execution of one Task.
+%
+% Assumes identification takes fixed amount of time, d.
+% Assumes sybil nodes are perfectly synchronized
+%  
 
 clear all
 
-N = 2000;
-B = 1990;
-k = 1;
-d = 0.15;
-R = 3;
+N = 2000;   % Total number of nodes
+B = 1990;   % Total number of Sybil nodes on ONE machine
+k = 1;      % Avgerage time to truthfully complete one task
+d = 0.15;   % Exact time for going through identification process
+R = 3;      % Number of replicas
 
-b = B/N;
-p = d/k;
+b = B/N;    % Ratio of Sybil nodes
+p = d/k;    % Ratio identification/task execution time
 
 % OBS! Måste använda ceiling:
 % ex R = 3; B = 1000
@@ -22,11 +35,25 @@ mceil = @(x) ceil(x);
 TimeForRFalse = d*mceil(R/B);
 Lr = (N-B)/(d+k);
 
+% How many truthful requests are sent in average per time interval
+% of length TimeForRFalse
+AverageTruthfulRequestsInInterval = Lr * TimeForRFalse
+
+% This gives the same results as below and are more intuitive,
+% the expression can be developed as below to get more general conclusions.
+% 
+% Calculated with Poisson distribution: f(0; Lr'), Lr' = Lr * TimeForRFalse
+% 
 %LogAllFalse1 = - Lr * TimeForRFalse
 %AllFalse1 = exp(LogAllFalse1)
 
 Freq = N*(1-b)*p/(p+1);
 C = mceil(R/(N*b));
 
-LogAllFalse2 = - Freq * C
-AllFalse2 = exp(LogAllFalse2)
+LogAllFalse2 = - Freq * C;
+ProbAllFalse = exp(LogAllFalse2)
+
+% Time wasted doing authentication
+ComputerWasteTimePerTask = R*d;
+% Actual efficiency
+ComputationalEfficiency = k / (k + ComputerWasteTimePerTask)
