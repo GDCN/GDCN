42c5fcb3ca8b72ddf551da03ba45c0ea098c85b0
Leif Schelin <scleif@student.chalmers.se>
2014-03-08 12:59:03 +0100

    Added doc

--- m_script/Sybil_prob.m
+++ m_script/Sybil_prob.m
@@ -2,7 +2,7 @@
 clear all
 
 N = 2000;
-B = 1000;
+B = 1990;
 k = 1;
 d = 0.15;
 R = 3;
@@ -10,9 +10,14 @@ R = 3;
 b = B/N;
 p = d/k;
 
-% Extremt känslig för Ceiling!!! 0.67 -> 2.2e-57 ...
-%mceil = @(x) ceil(x);
-mceil = @(x) (x);
+% OBS! Måste använda ceiling:
+% ex R = 3; B = 1000
+% ska bli 1 cykel, inte 0.003 cykler
+mceil = @(x) ceil(x);
+%mceil = @(x) (x);
+
+% OBS! Handlar inte om först till kvarn att ladda upp resultat
+% utan först till kvarn att vinna JobOwners respekt
 
 TimeForRFalse = d*mceil(R/B);
 Lr = (N-B)/(d+k);
