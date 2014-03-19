9baf373228f17a01a61d956e33321b3b6d9eb426
Leif Schelin <scleif@student.chalmers.se>
2014-03-19 11:07:00 +0100

    Added alternative mpi class using directDataRPC

--- GDCN_proj/Client/src/main/java/network/MessagePasser.java
+++ GDCN_proj/Client/src/main/java/network/MessagePasser.java
@@ -1,12 +1,10 @@
 package network;
 
-import net.tomp2p.connection.ConnectionBean;
-import net.tomp2p.connection.PeerBean;
 import net.tomp2p.message.Message;
 import net.tomp2p.p2p.BroadcastHandler;
 import net.tomp2p.p2p.Peer;
 import net.tomp2p.peers.Number160;
-import net.tomp2p.rpc.BroadcastRPC;
+import net.tomp2p.rpc.ReplyHandler;
 import net.tomp2p.storage.Data;
 
 import java.util.Map;
@@ -15,14 +13,6 @@ import java.util.Map;
  * Created by Leif on 2014-03-19.
  */
 public class MessagePasser {
-
-    /**
-     * Sets BroadcastRPC to our own extended version in order to handle custom message passing.
-     * @param node
-     */
-    public MessagePasser(Peer node){
-        node.setBroadcastRPC(new BRPC(node.getPeerBean(), node.getConnectionBean(), handler));
-    }
 
     /**
      * See tomp2p implementation at {@link net.tomp2p.p2p.DefaultBroadcastHandler#receive(net.tomp2p.message.Message)}
@@ -37,18 +27,22 @@ public class MessagePasser {
         }
     };
 
-    private class BRPC extends BroadcastRPC {
-        public BRPC(PeerBean peerBean, ConnectionBean connectionBean, BroadcastHandler broadcastHandler) {
-            super(peerBean, connectionBean, broadcastHandler);
+    private class CustomReplyHandler extends ReplyHandler {
+
+        public CustomReplyHandler(Peer node) {
+            super(node.getPeerBean(), node.getConnectionBean());
         }
 
         @Override
         public Message handleResponse(Message message, boolean sign) throws Exception {
             Message.Command cmd = message.getCommand();
             if (! (cmd == Message.Command.USER1 || cmd == Message.Command.USER2)) {
-                super.handleResponse(message, sign);
+                //TODO
+            } else {
+                //TODO
+                handler.receive(message);
             }
-            handler.receive(message);
+
             return message;
         }
     }
