c97413db8ba137f820df0fbd356e066ee6c09ecb
mr-ohman <ohmanjoakim@gmail.com>
2014-03-31 16:53:10 +0200

    Start of solution generation algorithm

--- GDCN_proj/Client/src/main/java/challenge/Solution.java
+++ GDCN_proj/Client/src/main/java/challenge/Solution.java
@@ -1,6 +1,9 @@
 package challenge;
 
 import java.io.Serializable;
+import java.util.Random;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by Leif on 2014-03-29.
@@ -13,8 +16,35 @@ public class Solution implements Serializable{
         this.key = key;
     }
 
-    public static Solution solve(Challenge challenge){
+    public static Solution solve(Challenge challenge) throws NoSuchAlgorithmException{
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        Random random = new Random();
+        byte[] rest;
+
+        do {
+            rest = new byte[random.nextInt(32)];
+            random.nextBytes(rest);
+            //md.update(key);
+            md.update(rest);
+        } while (checkZeros(md.digest(), 20)); //TODO Replace 20 with actual difficulty
+
         return new Solution(challenge.getKey());
+    }
+
+    private static boolean checkZeros(byte[] sha, int difficulty) {
+        int i;
+        for (i = 0; i < difficulty/8; i++) {
+            if (sha[i] != 0) {
+                return false;
+            }
+        }
+        byte b = 1;
+        for (int j = 0; j < difficulty%8; j++) {
+            if ((sha[i] & b) != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public String getKey() {
