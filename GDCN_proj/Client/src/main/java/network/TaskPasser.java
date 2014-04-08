41709ce6ce2d1e3b69ef770e2b2f0c84aa5743a6
Leif Schelin <schelinleif@gmail.com>
2014-04-08 12:12:59 +0200

    Reverted to previous hashcash values

--- GDCN_proj/Client/src/main/java/network/TaskPasser.java
+++ GDCN_proj/Client/src/main/java/network/TaskPasser.java
@@ -272,8 +272,8 @@ public class TaskPasser extends Passer {
 
                 Challenge challenge = workerNodeManager.isWorkerRegistered(workerID)?
                         hashCash.generateAuthenticationChallenge(myWorkerID, workerID)
-                        : hashCash.generateChallenge(HashCash.Purpose.REGISTER, myWorkerID.toString() + workerID.toString(), 15);
-//                        : hashCash.generateRegistrationChallenge(myWorkerID, workerID);
+//                        : hashCash.generateChallenge(HashCash.Purpose.REGISTER, myWorkerID.toString() + workerID.toString(), 15);
+                        : hashCash.generateRegistrationChallenge(myWorkerID, workerID);
                 return new TaskMessage(TaskMessageType.CHALLENGE, myWorkerID, challenge);
 
             case REQUEST_TASK:
