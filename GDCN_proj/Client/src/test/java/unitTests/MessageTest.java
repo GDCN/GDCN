17a250a645a756ee36559993691aecae17d48d7b
weeeeeew <jackpettersson@gmail.com>
2014-04-10 20:15:47 +0200

    Started tests for NetworkMessage

--- GDCN_proj/Client/src/test/java/unitTests/MessageTest.java
+++ GDCN_proj/Client/src/test/java/unitTests/MessageTest.java
@@ -4,20 +4,41 @@ import network.Crypto;
 import network.NetworkMessage;
 import org.testng.annotations.Test;
 
+import javax.crypto.SealedObject;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SignatureException;
+import java.util.Random;
+
 /**
  * Created by Leif on 2014-03-29.
  */
 public class MessageTest {
+    private KeyPairGenerator keygen;
+    private Random random;
+
+    public MessageTest() throws Exception {
+        keygen = KeyPairGenerator.getInstance("RSA");
+        random = new Random();
+    }
 
     @Test
-    public void encryptDecryptMessage(){
-        NetworkMessage message = new NetworkMessage("SomeData", NetworkMessage.Type.REQUEST);
+    public void encryptDecryptMessage() throws Exception {
+        KeyPair myKeyPair = keygen.generateKeyPair();
+        KeyPair otherKeyPair = keygen.generateKeyPair();
+        NetworkMessage message = new NetworkMessage(randomString(), NetworkMessage.Type.REQUEST);
 
-        Object encrypted = message.signAndEncrypt();
-        NetworkMessage decrypted = Crypto.decryptAndVerify(encrypted);
+        SealedObject encrypted = message.signAndEncrypt(myKeyPair.getPrivate(), otherKeyPair.getPublic());
+        NetworkMessage decrypted = NetworkMessage.decryptAndVerify(encrypted, otherKeyPair.getPrivate(), myKeyPair.getPublic());
 
         assert decrypted.getType() == message.getType();
         assert decrypted.getObject().equals(message.getObject());
     }
 
+    private String randomString() {
+        return new BigInteger(130, random).toString(32);
+    }
 }
