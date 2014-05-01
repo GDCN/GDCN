373adbb7ac9f87ef72625680290784bb8a2bb679
weeeeeew <jackpettersson@gmail.com>
2014-05-01 19:22:42 +0200

    Began fixing tests for Handshake with two keys

--- GDCN_proj/Client/src/test/java/unitTests/HandshakeTest.java
+++ GDCN_proj/Client/src/test/java/unitTests/HandshakeTest.java
@@ -4,39 +4,43 @@ import network.Crypto;
 import network.Handshake;
 import org.testng.annotations.Test;
 
+import javax.crypto.interfaces.DHPublicKey;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PublicKey;
 import java.security.SignedObject;
+import java.security.interfaces.RSAPublicKey;
 
 /**
  * Created by weeeeeew on 2014-04-10.
  */
 public class HandshakeTest {
-    private final KeyPairGenerator keygen;
+    private final KeyPairGenerator rsaKeygen,dhKeygen;
 
     public HandshakeTest() throws Exception {
-        keygen = KeyPairGenerator.getInstance("RSA");
+        rsaKeygen = KeyPairGenerator.getInstance("RSA");
+        dhKeygen = KeyPairGenerator.getInstance(Crypto.AGREEMENT_ALGORITHM);
     }
 
     @Test
     public void testReply() {
-        Handshake hs = new Handshake(keygen.generateKeyPair().getPublic());
+        Handshake hs1 = new Handshake((DHPublicKey) dhKeygen.generateKeyPair().getPublic(), (RSAPublicKey) rsaKeygen.generateKeyPair().getPublic());
+        Handshake hs2 = hs1.reply((DHPublicKey) dhKeygen.generateKeyPair().getPublic(), (RSAPublicKey) rsaKeygen.generateKeyPair().getPublic());
 
-        assert hs.reply(keygen.generateKeyPair().getPublic()) != null;
+        assert hs2 != null;
     }
 
     @Test
     public void testDoubleReply() {
-        Handshake hs1 = new Handshake(keygen.generateKeyPair().getPublic());
-        Handshake hs2 = hs1.reply(keygen.generateKeyPair().getPublic());
+        Handshake hs1 = new Handshake(rsaKeygen.generateKeyPair().getPublic());
+        Handshake hs2 = hs1.reply(rsaKeygen.generateKeyPair().getPublic());
 
-        assert hs2.reply(keygen.generateKeyPair().getPublic()) == null;
+        assert hs2.reply(rsaKeygen.generateKeyPair().getPublic()) == null;
     }
 
     @Test
     public void testPublicKey() {
-        PublicKey key = keygen.generateKeyPair().getPublic();
+        PublicKey key = rsaKeygen.generateKeyPair().getPublic();
         Handshake hs = new Handshake(key);
 
         assert hs.dhKey.equals(key);
@@ -44,8 +48,8 @@ public class HandshakeTest {
 
     @Test
     public void testReplyPublicKey() {
-        PublicKey key1 = keygen.generateKeyPair().getPublic();
-        PublicKey key2 = keygen.generateKeyPair().getPublic();
+        PublicKey key1 = rsaKeygen.generateKeyPair().getPublic();
+        PublicKey key2 = rsaKeygen.generateKeyPair().getPublic();
         Handshake hs1 = new Handshake(key1);
         Handshake hs2 = hs1.reply(key2);
 
@@ -54,8 +58,8 @@ public class HandshakeTest {
 
     @Test
     public void testSignatureValid() throws Exception {
-        PublicKey key = keygen.generateKeyPair().getPublic();
-        KeyPair keypair = keygen.generateKeyPair();
+        PublicKey key = rsaKeygen.generateKeyPair().getPublic();
+        KeyPair keypair = rsaKeygen.generateKeyPair();
         Handshake hs = new Handshake(key);
         SignedObject signedHS = hs.sign(keypair.getPrivate());
 
@@ -64,7 +68,7 @@ public class HandshakeTest {
 
     @Test
     public void testSignedObject() throws Exception {
-        KeyPair keypair = keygen.generateKeyPair();
+        KeyPair keypair = rsaKeygen.generateKeyPair();
         Handshake hs = new Handshake(keypair.getPublic());
         SignedObject signedHS = hs.sign(keypair.getPrivate());
 
