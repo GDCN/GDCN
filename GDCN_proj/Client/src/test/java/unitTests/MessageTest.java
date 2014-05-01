409b0bc7a38794ed15de9b1b947cf27a2278783d
weeeeeew <jackpettersson@gmail.com>
2014-05-01 19:21:44 +0200

    Amended tests for NetworkMessage with symmetric crypto

--- GDCN_proj/Client/src/test/java/unitTests/MessageTest.java
+++ GDCN_proj/Client/src/test/java/unitTests/MessageTest.java
@@ -1,9 +1,12 @@
 package unitTests;
 
+import network.Crypto;
 import network.NetworkMessage;
 import org.testng.annotations.Test;
 
+import javax.crypto.KeyGenerator;
 import javax.crypto.SealedObject;
+import javax.crypto.SecretKey;
 import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -13,24 +16,26 @@ import java.util.Random;
  * Created by Leif on 2014-03-29.
  */
 public class MessageTest {
-    private KeyPairGenerator keygen;
+    private KeyGenerator keygen;
     private Random random;
 
     public MessageTest() throws Exception {
-        keygen = KeyPairGenerator.getInstance("RSA");
+        keygen = KeyGenerator.getInstance(Crypto.ENCRYPTION_ALGORITHM);
         random = new Random();
     }
 
     @Test
     public void encryptDecryptMessage() throws Exception {
-        KeyPair myKeyPair = keygen.generateKeyPair();
-        KeyPair otherKeyPair = keygen.generateKeyPair();
+        SecretKey myKey = keygen.generateKey();
+        SecretKey wrongKey = keygen.generateKey();
         NetworkMessage message = new NetworkMessage(randomString(), NetworkMessage.Type.REQUEST);
 
-        SealedObject encrypted = message.encrypt(myKeyPair.getPrivate(), otherKeyPair.getPublic());
-        NetworkMessage decrypted = NetworkMessage.decrypt(encrypted, otherKeyPair.getPrivate(), myKeyPair.getPublic());
+        SealedObject encrypted = message.encrypt(myKey);
+        NetworkMessage decrypted = NetworkMessage.decrypt(encrypted, myKey);
 
         assert decrypted.equals(message);
+
+        assert NetworkMessage.decrypt(encrypted,wrongKey) != null;
     }
 
     private String randomString() {
