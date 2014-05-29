80666c5e42402c05538863a38a20edbf4f11c101
Leif Schelin <schelinleif@gmail.com>
2014-05-29 14:03:26 +0200

    Cleaning in Install

--- GDCN_proj/TaskBuilder/src/main/java/se/chalmers/gdcn/taskbuilder/fileManagement/Install.java
+++ GDCN_proj/TaskBuilder/src/main/java/se/chalmers/gdcn/taskbuilder/fileManagement/Install.java
@@ -1,24 +1,11 @@
 package se.chalmers.gdcn.taskbuilder.fileManagement;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import se.chalmers.gdcn.taskbuilder.ExitFailureException;
 
 import java.io.*;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.CodeSource;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.jar.JarInputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,6 +26,8 @@ public class Install {
 
     public static final String LIB_DIR = APPDATA + "lib";
     public static final String HDB_DIR = APPDATA + "hdb.conf.d";
+
+    public static final String STD_BIN = APPDATA + "bin" + SEPARATOR;
 
     //private constructor
     private Install(){
@@ -85,7 +74,7 @@ public class Install {
         }
 
         if (pathData != null) {
-            installHaskellLibraries(pathData.getProperty("bin_path"));
+            installHaskellLibraries();
         }
 
         writeBootstrapNodes();
@@ -133,9 +122,9 @@ public class Install {
     private static Properties paths(){
         Properties props = new Properties();
 
-        String subHeaderPath = "TaskBuilder" + SEPARATOR + "src" + SEPARATOR + "main" + SEPARATOR + "haskell" + SEPARATOR;
-
-        props.put("bin_path", System.getProperty("user.dir") + SEPARATOR + subHeaderPath);
+//        String subHeaderPath = "TaskBuilder" + SEPARATOR + "src" + SEPARATOR + "main" + SEPARATOR + "haskell" + SEPARATOR;
+//        props.put("bin_path", System.getProperty("user.dir") + SEPARATOR + subHeaderPath);
+        props.put("bin_path", STD_BIN);
         props.put("data_path", APPDATA + "data" + File.separator);
         props.put("job_path", APPDATA + "jobs" + File.separator);
         props.put("settings_path", APPDATA + "settings" + File.separator);
@@ -143,172 +132,9 @@ public class Install {
         return props;
     }
 
-    /**
-     * Extracts haskell dirs in jar file.
-     * @return {@link java.io.File} to extracted directory
-     */
-    private static File extractHaskellDirs(){
-        Install obj = new Install();
-        URL resource = obj.getClass().getResource("/haskell");
-        System.out.println("HaskellFolder: " + resource);
-
-        File r = null;
-        try {
-            r = new File(resource.toURI());
-            System.out.println("URI successful: "+r.getAbsolutePath());
-        } catch (Exception e) {
-            //URISyntaxException?
-            e.printStackTrace();
-
-            try {
-                r = new File(resource.getPath());
-                System.out.println("String successful: "+r.getAbsolutePath());
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-        }
-
-        File haskellDir = new File(APPDATA + SEPARATOR + "tempHaskell");
-        haskellDir.mkdirs();
-//        InputStream resourceStream = obj.getClass().getResourceAsStream("/haskell");
-
-        try {
-            FileUtils.copyDirectory(r, haskellDir, false);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-
-        return haskellDir;
-    }
-
-    private static boolean extractHaskellDirs2(File targetDir){
-        targetDir.mkdirs();
-
-//        Install obj = new Install();
-        URL resource = Thread.currentThread().getClass().getResource("/haskell");
-        System.out.println("HaskellFolder URL: " + resource);
-
-        URI res = null;
-        try {
-             res = resource.toURI();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        System.out.println("HaskellFolder URI: " + res);
-
-        Map<String, String> env = new HashMap<>();
-        try {
-//            FileSystem fileSystem = FileSystems.newFileSystem(res, env);
-            FileSystem fileSystem = FileSystems.newFileSystem(res, env);
-//            Path path = fileSystem.getPath(".");
-            Path path = Paths.get(res);
-            System.out.println("Paths: "+Paths.get(res));
-            System.out.println("FileSystem: "+fileSystem.getPath("/haskell"));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-
-//        InputStream resourceStream = obj.getClass().getResourceAsStream("/haskell");
-//        FileInputStream fileInputStream = new FileInputStream(resourceStream);
-
-//        try {
-//            FileUtils.copyDirectory(r, haskellDir, false);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-
-        //todo fix
-        return false;
-    }
-
-    private static void extractHaskellDirs5(){
-        Install obj = new Install();
-        CodeSource codeSource = obj.getClass().getProtectionDomain().getCodeSource();
-        URL location = codeSource.getLocation();
-        System.out.println("URL: "+ location);
-
-        try {
-            JarFile jarFile = new JarFile(location.getFile());
-
-            JarEntry haskellEntry = jarFile.getJarEntry("haskell");
-            if(haskellEntry == null){
-                System.out.println("HaskellEntry null!!!");
-            }
-
-            File targetHaskellDir = new File(APPDATA + SEPARATOR + "tempHaskell");
-            File targetFile = new File(targetHaskellDir + SEPARATOR + haskellEntry.getName());
-
-
-            InputStream inputStream = jarFile.getInputStream(haskellEntry);
-            FileOutputStream fileOutputStream = new FileOutputStream(targetFile);
-            while(inputStream.available() > 0){
-                fileOutputStream.write(inputStream.read());
-            }
-
-            inputStream.close();
-            fileOutputStream.close();
-
-            //            Enumeration<JarEntry> entries = jarFile.entries();
-//            while (entries.hasMoreElements()){
-//                JarEntry entry = entries.nextElement();
-//            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    private static void extractHaskellDirs4(){
-        File targetHaskellDir = new File(APPDATA + SEPARATOR + "tempHaskell");
-        Install obj = new Install();
-        CodeSource codeSource = obj.getClass().getProtectionDomain().getCodeSource();
-        URL location = codeSource.getLocation();
-
-        System.out.println("Location: "+location);
-
-        JarInputStream jarInputStream = null;
-        try{
-            jarInputStream = new JarInputStream(new FileInputStream(location.getFile()));
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (jarInputStream != null) {
-                try {
-                    jarInputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-//        ZipInputStream zipInputStream = new ZipInputStream(location.getFile());
-
-
-    }
-
-    private static void extractHaskellDirs3(){
-        File targetHaskellDir = new File(APPDATA + SEPARATOR + "tempHaskell");
-
-//        URL resource = Thread.currentThread().getContextClassLoader().getResource("haskell");
-//        System.out.println("URL resource: "+resource);
-
-        InputStream resourceStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("haskell");
-
-    }
-
-    private static void installHaskellLibraries(String bin_path) {
-        File targetHaskellDir = new File(APPDATA + SEPARATOR + "tempHaskell");
-        String subdir = "haskell";
+    private static void installHaskellLibraries() {
+        final File targetHaskellDir = new File(STD_BIN);
+        final String subdir = "haskell";
 
         try {
             Install obj = new Install();
@@ -319,7 +145,6 @@ public class Install {
             System.out.println("Location URL: "+location);
 
             File jar = new File(location.toURI());
-
             JarExtractor.extract(jar, subdir, targetHaskellDir);
 
         } catch (Exception e) {
