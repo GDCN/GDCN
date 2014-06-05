c7f5f4bf63af01d88ff8829b82c4ccb45edefa87
Leif Schelin <schelinleif@gmail.com>
2014-06-05 15:46:32 +0200

    Added changed files

---
+++ GDCN_proj/Client/src/main/java/se/chalmers/gdcn/network/Handshake.java
@@ -0,0 +1,52 @@
+package se.chalmers.gdcn.network;
+
+import java.io.Serializable;
+import java.security.PublicKey;
+
+/**
+ * Created by weeeeeew on 2014-04-10.
+ */
+public class Handshake implements Serializable {
+    public final PublicKey agreementKey, signKey;
+    public final Phase phase;
+
+    /**
+     * Creates a new Handshake in the initial phase, containing the specified keys.
+     * @param agreementKey The public key of the key agreement algorithm.
+     * @param signKey The public key of the digital signature algorithm.
+     */
+    public Handshake(PublicKey agreementKey, PublicKey signKey) {
+        this(agreementKey, signKey, Phase.INIT);
+    }
+
+    private Handshake(PublicKey agreementKey, PublicKey signKey, Phase phase) {
+        this.agreementKey = agreementKey;
+        this.signKey = signKey;
+        this.phase = phase;
+    }
+
+    /**
+     * Creates a replying handshake, using the specified keys.
+     * @param agreementKey The public key of the key agreement algorithm.
+     * @param signKey The public key of the digital signature algorithm.
+     * @return A new handshake in the next phase.
+     */
+    public Handshake reply(PublicKey agreementKey, PublicKey signKey) {
+        return phase == Phase.INIT ? new Handshake(agreementKey,signKey, Phase.REPLY) : null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Handshake) {
+            Handshake hs = (Handshake) o;
+            return this.agreementKey.equals(hs.agreementKey) && this.signKey.equals(hs.signKey) && this.phase == hs.phase;
+        }
+
+        return false;
+    }
+
+    public enum Phase {
+        INIT,
+        REPLY
+    }
+}
---
+++ GDCN_proj/Client/src/main/java/se/chalmers/gdcn/network/aNetworkMessage.java
@@ -0,0 +1,66 @@
+package se.chalmers.gdcn.network;
+
+import javax.crypto.*;
+import java.io.IOException;
+import java.io.Serializable;
+import java.security.*;
+
+/**
+ * Created by Leif on 2014-03-24.
+ *
+ * General purpose class used for any message passing.
+ */
+public class aNetworkMessage implements Serializable {
+
+    private final Serializable object;
+    private final Type type;
+
+    public NetworkMessage(Serializable object, Type type) {
+        this.object = object;
+        this.type = type;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public byte[] encrypt(SecretKey key) throws InvalidKeyException {
+        return Crypto.encrypt(this, key);
+    }
+
+    public static NetworkMessage decrypt(byte[] ciphertext, SecretKey key) throws InvalidKeyException {
+        Serializable data = Crypto.decrypt(ciphertext, key);
+
+        if (data instanceof NetworkMessage) {
+            return (NetworkMessage) data;
+        } else if (data == null) {
+            return null;
+        } else {
+            throw new InvalidParameterException("The encrypted object was not a NetworkMessage");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "NetwM{ " + type +
+                ", " + object +
+                '}';
+    }
+
+    public static enum Type {
+        REQUEST,
+        NO_REPLY
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof NetworkMessage
+                ? this.object.equals( ((NetworkMessage) o).object ) && this.type == ((NetworkMessage) o).type
+                : false;
+    }
+
+}
