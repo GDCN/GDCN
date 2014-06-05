9fd69f7c833ae875fac4dd91a71c253e5d04ec68
Leif Schelin <schelinleif@gmail.com>
2014-06-05 16:07:05 +0200

    Added unit tests, may have found source of problem

---
+++ GDCN_proj/Client/src/test/java/se/chalmers/gdcn/tests/CryptoTest.java
@@ -0,0 +1,92 @@
+package se.chalmers.gdcn.tests;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import se.chalmers.gdcn.network.Crypto;
+
+import javax.crypto.KeyAgreement;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.math.BigInteger;
+import java.security.*;
+import java.util.Random;
+
+/**
+ * Created by weeeeeew on 2014-04-10.
+ */
+public class CryptoTest {
+    private KeyGenerator secretKeygen;
+    private KeyPairGenerator publicKeygen;
+    private Random random;
+
+    @BeforeClass
+    public void initialize() throws Exception {
+        secretKeygen = KeyGenerator.getInstance(Crypto.ENCRYPTION_ALGORITHM);
+        publicKeygen = KeyPairGenerator.getInstance(Crypto.SIGNATURE_KEY_ALGORITHM);
+        random = new Random();
+        //TODO give seed. Tests should be fully deterministic even when using Random. can run same test multiple times instead.
+    }
+
+    @Test
+     public void testEncryptDecrypt() throws Exception {
+        SecretKey key = secretKeygen.generateKey();
+        String message = randomString();
+        byte[] encMsg = Crypto.encrypt(message, key);
+
+        assert message.equals(Crypto.decrypt(encMsg,key));
+
+        SecretKey wrongKey = secretKeygen.generateKey();
+
+        assert Crypto.decrypt(encMsg,wrongKey) == null;
+
+        encMsg[1] = (byte) (encMsg[1] == 0 ? 1 : 0);
+
+        assert Crypto.decrypt(encMsg,key) == null;
+    }
+
+    @Test
+    public void testSignVerify() throws Exception {
+        KeyPair keypair = publicKeygen.generateKeyPair();
+        SignedObject signedObject = Crypto.sign(randomString(), keypair.getPrivate());
+
+        assert Crypto.verify(signedObject, keypair.getPublic());
+
+        PublicKey wrongKey = publicKeygen.generateKeyPair().getPublic();
+
+        assert !Crypto.verify(signedObject, wrongKey);
+    }
+
+    @Test
+    public void testSignedCorrectObject() throws Exception {
+        PrivateKey privateKey = publicKeygen.generateKeyPair().getPrivate();
+        String message = randomString();
+        SignedObject signedObject = Crypto.sign(message, privateKey);
+
+        assert message.equals(signedObject.getObject());
+    }
+
+
+    @Test
+    public void testAgreement() throws Exception {
+        KeyAgreement backupAgreement = KeyAgreement.getInstance(Crypto.AGREEMENT_ALGORITHM);
+        KeyPairGenerator backupKeygen = KeyPairGenerator.getInstance(Crypto.AGREEMENT_ALGORITHM);
+
+        KeyPair keyPair1 = Crypto.generateAgreementKeyPair();
+        KeyPair keyPair2 = backupKeygen.generateKeyPair();
+
+        assert !keyPair1.equals(keyPair2);
+
+        SecretKey secretKey1 = Crypto.generateSecretKey(keyPair1.getPrivate(),keyPair2.getPublic());
+
+        backupAgreement.init(keyPair2.getPrivate());
+        backupAgreement.doPhase(keyPair1.getPublic(),true);
+
+        SecretKey secretKey2 = backupAgreement.generateSecret(Crypto.ENCRYPTION_ALGORITHM);
+
+        assert secretKey1.equals(secretKey2);
+    }
+
+    private String randomString() {
+        return new BigInteger(130, random).toString(32);
+    }
+}
---
+++ GDCN_proj/Client/src/test/java/se/chalmers/gdcn/tests/HandshakeTest.java
@@ -0,0 +1,72 @@
+package se.chalmers.gdcn.tests;
+
+import org.testng.annotations.Test;
+import se.chalmers.gdcn.network.Crypto;
+import se.chalmers.gdcn.network.Handshake;
+
+import java.security.KeyPairGenerator;
+import java.security.PublicKey;
+
+/**
+ * Created by weeeeeew on 2014-04-10.
+ */
+public class HandshakeTest {
+    private final KeyPairGenerator rsaKeygen,dhKeygen;
+
+    //TODO why use constructor? Standard is to use @BeforeClass and/or @BeforeMethod setup method.
+    public HandshakeTest() throws Exception {
+        rsaKeygen = KeyPairGenerator.getInstance("RSA");
+        dhKeygen = KeyPairGenerator.getInstance(Crypto.AGREEMENT_ALGORITHM);
+    }
+
+    @Test
+    public void testReply() {
+        Handshake hs1 = new Handshake(dhKeygen.generateKeyPair().getPublic(), rsaKeygen.generateKeyPair().getPublic());
+        Handshake hs2 = hs1.reply(dhKeygen.generateKeyPair().getPublic(), rsaKeygen.generateKeyPair().getPublic());
+
+        assert hs2 != null;
+        assert !hs1.equals(hs2);
+        assert hs2.reply(dhKeygen.generateKeyPair().getPublic(), rsaKeygen.generateKeyPair().getPublic()) == null;
+    }
+
+    @Test
+    public void testPublicKey() {
+        PublicKey rsaKey1 = rsaKeygen.generateKeyPair().getPublic();
+        PublicKey dhKey1 = dhKeygen.generateKeyPair().getPublic();
+
+        Handshake hs = new Handshake(dhKey1,rsaKey1);
+
+        assert hs.agreementKey.equals(dhKey1) && hs.signKey.equals(rsaKey1);
+
+        PublicKey rsaKey2 = rsaKeygen.generateKeyPair().getPublic();
+        PublicKey dhKey2 = dhKeygen.generateKeyPair().getPublic();
+
+        Handshake hs2 = hs.reply(dhKey2,rsaKey2);
+
+        assert hs2.signKey.equals(rsaKey2) && hs2.agreementKey.equals(dhKey2);
+        assert !hs2.signKey.equals(rsaKey1) && !hs2.agreementKey.equals(dhKey1);
+    }
+
+    @Test
+    public void testEquality() {
+        PublicKey dhKey1 = dhKeygen.generateKeyPair().getPublic();
+        PublicKey rsaKey1 = rsaKeygen.generateKeyPair().getPublic();
+
+        Handshake hs1 = new Handshake(dhKey1,rsaKey1);
+        Handshake hs2 = new Handshake(dhKey1,rsaKey1);
+
+        assert hs1.equals(hs2) && hs2.equals(hs1);
+        assert !hs1.equals(hs1.reply(dhKey1,rsaKey1)) && !hs1.reply(dhKey1,rsaKey1).equals(hs1);
+
+        PublicKey dhKey2 = dhKeygen.generateKeyPair().getPublic();
+        PublicKey rsaKey2 = rsaKeygen.generateKeyPair().getPublic();
+
+        Handshake hs3 = new Handshake(dhKey1,rsaKey2);
+        Handshake hs4 = new Handshake(dhKey2,rsaKey1);
+        Handshake hs5 = new Handshake(dhKey2,rsaKey2);
+
+        assert !hs1.equals(hs3) && !hs3.equals(hs1);
+        assert !hs1.equals(hs4) && !hs4.equals(hs1);
+        assert !hs1.equals(hs5) && !hs5.equals(hs1);
+    }
+}
